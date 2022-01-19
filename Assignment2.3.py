9f35ebc6286c8727f986a2609d52753d7596c2bd
David Powell <97639705+generaldap15@users.noreply.github.com>
2022-01-18 19:45:10 -0500

    Add files via upload
    Uploaded Homework

---
+++ Assignment2.3.py
@@ -0,0 +1,4 @@
+hours = int(input("Enter Hours: "))
+rate = int(input("Enter Rate:"))
+pay = hours * rate
+print ("You made"  ,pay , "dollars")
---
+++ Assignment2.5.py
@@ -0,0 +1,3 @@
+Celcius = float(input ("Temperture in Celcius: "))
+Fare = (Celcius * 1.8 + 32)
+print ("Temperture in fahrenheit:" ,Fare)
